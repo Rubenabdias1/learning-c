83c3b5bfc770ae687c26ae71adfb78ecfea1e177
Ruben Nunez <ruben_abdias@live.com>
2023-09-16 23:28:56 -0500

    printf

---
+++ src/printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int favNum = 90;
+    char myChar = 'i';
+
+    printf("Hello\"World");
+    printf("My favorite %s is %d", myChar, favNum);
+
+    return 0;
+}
